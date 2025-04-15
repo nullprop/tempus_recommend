9758524f792c86eb0dcf667dc8d3dfe9e987669c
nullprop <git@nullprop.sh>
2025-04-15 23:32:42 +0300

    Scale recommendations by map rating

--- recommend.py
+++ recommend.py
@@ -1,8 +1,19 @@
 import map_classification as mc
 
 
+def _rating_scale(rating):
+    if rating == 1:
+        return 1.0
+    elif rating == 2:
+        return 0.7
+    elif rating == 3:
+        return 0.2
+    return 0
+
+
 def recommend_player(player_id, class_id, max_rank):
     mc.load_model(class_id)
+    maps = mc.get_maps()
     tts = mc.get_player_tts(player_id, class_id, max_rank)
 
     if len(tts) == 0:
@@ -18,9 +29,12 @@ def recommend_player(player_id, class_id, max_rank):
             else:
                 recommendations[s["name"]] = s["value"]
 
+    class_rating = "srating" if class_id == 3 else "drating"
+
     recommendations_list = []
     for k, v in recommendations.items():
-        recommendations_list.append({"name": k, "value": v})
+        map = [x for x in maps if x["name"] == k][0]
+        recommendations_list.append({"name": k, "value": v * _rating_scale(map[class_rating])})
     recommendations_list.sort(key=lambda r: -r["value"])
     return recommendations_list
 
